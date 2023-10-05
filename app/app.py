536941b6bfc458789c8a6a7a4caf5ce3980ef9b9
shuminchang <magic2313@gmail.com>
2023-10-05 17:23:21 +0800

    add page for drug review predict

--- app/app.py
+++ app/app.py
@@ -1,18 +1,67 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, session
 import numpy as np
 import joblib
 import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
 import uuid
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+from bs4 import BeautifulSoup
+import re
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
+# Set the secret key to some random bytes. Keep this really secret!
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+
+MODEL_PATH = 'model/passmodel.pkl'
+TOKENIZER_PATH = 'model/tfidfvectorizer.pkl'
+DATA_PATH = 'drug/drugsComTrain.tsv'
+
+vectorizer = joblib.load(TOKENIZER_PATH)
+model = joblib.load(MODEL_PATH)
+
+stop = stopwords.words('english')
+lemmatizer = WordNetLemmatizer()
+
+@app.route('/')
+def login():
+    return render_template('login.html')
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect('/')
+
+@app.route('/index')
+def index():
+    if 'user_id' in session:
+        return render_template('home.html')
+    else:
+        return redirect('/')
+
+@app.route('/login_validation', methods=['POST'])
+def login_validation():
+    username = request.form.get('username')
+    password = request.form.get('password')
+
+    session['user_id'] = username
+    session['domain'] = password
+
+    if username == "admin@gmail.com" and password == "admin":
+        return render_template('home.html')
+    else:
+        err = "Kindly Enter valid User ID/Password"
+        return render_template('login.html', lbl=err)
+    
+    return ""
+
+@app.route('/age', methods=['GET', 'POST'])
 def hello_world():
     request_type_str = request.method
     if request_type_str == 'GET':
-        return render_template('index.html', href='static/base_pic.svg')
+        return render_template('age.html', href='static/base_pic.svg')
     else:
         text = request.form['text']
         random_string = uuid.uuid4().hex
@@ -21,8 +70,51 @@ def hello_world():
         np_arr = floats_string_to_input_arr(text)
         make_picture('AgesAndHeights.pkl', model, np_arr, path)
 
-        return render_template('index.html', href=path)
-    
+        return render_template('age.html', href=path)
+
+@app.route('/predict', methods=['GET', 'POST'])
+def predict():
+    request_type_str = request.method
+    if request_type_str == 'GET':
+        return render_template('predict.html')
+    else:
+        raw_text = request.form['rawtext']
+
+        if raw_text != "":
+            clean_text = cleanText(raw_text)
+            clean_list = [clean_text]
+
+            tfidf_vect = vectorizer.transform(clean_list)
+            prediction = model.predict(tfidf_vect)
+            predicted_cond = prediction[0]
+            df = pd.read_csv(DATA_PATH, sep='\t')
+            top_drugs = top_drugs_extractor(predicted_cond, df)
+            
+            return render_template('predict.html', raw_text = raw_text, 
+                                                   result = predicted_cond, 
+                                                   top_drugs = top_drugs)
+        else:
+            raw_text = "There is no text to select"
+
+def cleanText(raw_review):
+    # 1. Delete HTML
+    review_text = BeautifulSoup(raw_review, 'html.parser').get_text()
+    # 2. Make a space
+    letters_only = re.sub('[^a-zA-Z]', ' ', review_text)
+    # 3. lower letters
+    words = letters_only.lower().split()
+    # 4. Stopwords
+    meaningful_words = [w for w in words if not w in stop]
+    # 5. lemmitization
+    lemmatize_words = [lemmatizer.lemmatize(w) for w in meaningful_words]
+    # 6. space join words
+    return ( ' '.join(lemmatize_words) )
+
+def top_drugs_extractor(condition, df):
+    df_top = df[(df['rating'] >= 9) & (df['usefulCount'] >= 100)].sort_values(by = ['rating', 'usefulCount'], ascending = [False, False])
+    drug_list = df_top[df_top['condition'] == condition]['drugName'].head(3).tolist()
+    return drug_list
+
 def make_picture(training_data_filename, model, new_input_arr, output_file):
     # Plot training data with model
     data = pd.read_pickle(training_data_filename)
@@ -63,3 +155,6 @@ def floats_string_to_input_arr(floats_str):
     floats = [float(x.strip()) for x in floats_str.split(',') if is_float(x)]
     as_np_arr = np.array(floats).reshape(len(floats), 1)
     return as_np_arr
+
+if __name__ == "__main__":
+    app.run(debug=True)
